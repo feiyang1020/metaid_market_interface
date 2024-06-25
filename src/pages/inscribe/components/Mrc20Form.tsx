be2573c25c26ec2059bd61dcd262ae8dd34d696f
feiyang1020 <feiyangl1020@163.com>
2024-06-25 17:14:54 +0800

    fix

--- src/pages/inscribe/components/Mrc20Form.tsx
+++ src/pages/inscribe/components/Mrc20Form.tsx
@@ -590,7 +590,7 @@ export default ({ setTab }: { setTab: (tab: string) => void }) => {
                                                         </Row></Checkbox.Group>
                                                 </Form.Item></Col></Row> : <Row gutter={[0, 0]}>
                                             <Col offset={sm ? 5 : 0} span={sm ? 19 : 24}><div className="noPins" onClick={() => { history.push('/') }}><FileTextOutlined style={{ fontSize: 36 }} /><div>
-                                                No PIN. GO get it
+                                                No PIN. Go get it
                                             </div></div></Col></Row>
                                     }
                                 </>}
