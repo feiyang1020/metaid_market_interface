ac6e81ec498f9073261bac406d4196fbf803fead
feiyang1020 <feiyangl1020@163.com>
2024-06-26 19:10:33 +0800

    fix

--- src/utils/mempool.ts
+++ src/utils/mempool.ts
@@ -7,24 +7,46 @@ export const getFeeRate = async (network: API.Network) => {
     hostname: "mempool.space",
     network: network === "mainnet" ? "main" : "testnet",
   });
+  try{
+    const feesRecommended = await fees.getFeesRecommended();
+    console.log(feesRecommended);
+    return [
+      {
+        label: "Fast",
+        value: feesRecommended.fastestFee,
+        time: "15 minutes",
+      },
+      {
+        label: "Avg",
+        value: feesRecommended.halfHourFee,
+        time: "30 minutes",
+      },
+      {
+        label: "Slow",
+        value: feesRecommended.hourFee,
+        time: "about 1 hour",
+      },
+    ];
+  }catch(e){
+    console.log(e);
+    return [
+      {
+        label: "Fast",
+        value: 0,
+        time: "15 minutes",
+      },
+      {
+        label: "Avg",
+        value: 0,
+        time: "30 minutes",
+      },
+      {
+        label: "Slow",
+        value: 0,
+        time: "about 1 hour",
+      },
+    ];
+  }
 
-  const feesRecommended = await fees.getFeesRecommended();
-  console.log(feesRecommended);
-  return [
-    {
-      label: "Fast",
-      value: feesRecommended.fastestFee,
-      time: "15 minutes",
-    },
-    {
-      label: "Avg",
-      value: feesRecommended.halfHourFee,
-      time: "30 minutes",
-    },
-    {
-      label: "Slow",
-      value: feesRecommended.hourFee,
-      time: "about 1 hour",
-    },
-  ];
+  
 };
