1c32fb2d617eab1b55806fe5aa109c2cde8759d6
feiyang1020 <feiyangl1020@163.com>
2024-07-18 20:26:34 +0800

    fix

--- src/components/Mrc20List/index.tsx
+++ src/components/Mrc20List/index.tsx
@@ -4,6 +4,8 @@ import AllList from "./AllList";
 import "./index.less";
 import Minting from "./Minting";
 import IdCoins from "./IdCoins";
+import { useSearchParams, useNavigate, useResolvedPath } from "umi";
+import { useEffect, useState } from "react";
 const { useBreakpoint } = Grid;
 const items: TabsProps['items'] = [
     {
@@ -25,6 +27,15 @@ const items: TabsProps['items'] = [
 ];
 export default () => {
     const screens = useBreakpoint();
+    const [activeKey, setActiveKey] = useState('0');
+    const nav = useNavigate()
+    const [query] = useSearchParams();
+    const _tab = query.get("mt");
+    useEffect(() => {
+        if (_tab) {
+            setActiveKey(_tab)
+        }
+    }, [_tab]);
     return <ConfigProvider
         theme={{
             components: {
@@ -39,5 +50,7 @@ export default () => {
                 }
             },
         }}
-    ><Tabs tabBarStyle={{ paddingLeft: screens.lg ? 20 : 20 }} className="mrc20ListWrap" defaultActiveKey="0" items={items} /></ConfigProvider>
+    ><Tabs onChange={(item) => {
+        nav({ search: `?tab=MRC-20&mt=${item}` })
+    }} tabBarStyle={{ paddingLeft: screens.lg ? 20 : 20 }} className="mrc20ListWrap" activeKey={activeKey} defaultActiveKey="0" items={items} /></ConfigProvider>
 }
