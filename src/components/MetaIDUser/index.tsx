7ed7d9aa8e00cd7070343bfd4f9f4bbdfc277578
feiyang1020 <feiyangl1020@163.com>
2024-07-31 18:13:30 +0800

    fix

--- src/components/MetaIDUser/index.tsx
+++ src/components/MetaIDUser/index.tsx
@@ -14,7 +14,7 @@ type Props = {
 export default ({ avatar, avatarSize = 72, name, address, metaid, className = '' }: Props) => {
     const { network } = useModel('wallet')
     return <div className={"metaidUserWrapCom " + className} onClick={() => window.open(getMetaIdUrlByNet(network) + metaid, '_blank')}>
-        <MetaIdAvatar avatar={avatar} size={avatarSize} />
+        <MetaIdAvatar avatar={avatar} size={avatarSize} style={{minWidth:avatarSize}} />
         <div className="nameWrap">
             <div className="name">
                 {name || address.replace(/(\w{5})\w+(\w{5})/, "$1...$2")}
